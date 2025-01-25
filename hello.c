06e5add2d04d94672136c15a2dbf4d1df7dc8fff
= <lukasilverio94@gmail.com>
2025-01-26 00:29:14 +0100

    simple printf

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void){
-    printf("papobinario.com.br\n");
-    return 0;
+   int ret = 0;
+    
+   printf("papobinario.com.br\n");
+   printf("retorno da print() anterior: %d\n", ret);
+   printf("%x - %x - %x - %c\n", ret, 10, 0xa, 'A');
+   return 0;
 }
 
